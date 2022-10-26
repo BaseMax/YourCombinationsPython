35e51ef5e985e6fb787338c2ce62487d90e5ffbe
Max Base <maxbasecode@gmail.com>
2022-10-26 19:17:46 +0200

    comment bad commands

--- example.py
+++ example.py
@@ -31,16 +31,16 @@ for i in your_combinations.permutations(2):
 ##################################################################################
 
 # power set
-print("Power set:", list(your_combinations.powerSet()))
+# print("Power set:", list(your_combinations.powerSet()))
 
 # combinations with repetition
-print("Combinations with repetition:", list(your_combinations.combinations(2, True)))
+# print("Combinations with repetition:", list(your_combinations.combinations(2, True)))
 
 # combinations without repetition
-print("Combinations without repetition:", list(your_combinations.combinations(2)))
+# print("Combinations without repetition:", list(your_combinations.combinations(2)))
 
 # permutations with repetition
-print("Permutations with repetition:", list(your_combinations.permutations(2, True)))
+# print("Permutations with repetition:", list(your_combinations.permutations(2, True)))
 
 # permutations without repetition
-print("Permutations without repetition:", list(your_combinations.permutations(2)))
+# print("Permutations without repetition:", list(your_combinations.permutations(2)))
