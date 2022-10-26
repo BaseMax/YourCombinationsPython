5791024f4459501504f9541497d75d03ba48750c
Max Base <maxbasecode@gmail.com>
2022-10-26 19:08:40 +0200

    .

--- example.py
+++ example.py
@@ -4,21 +4,38 @@ from YourCombinations import YourCombinations
 your_combinations = YourCombinations([1, 2, 3])
 
 # power set
-for i in your_combinations.powerSet([1, 2, 3]):
-    print(i)
+print("Power set:", list(your_combinations.powerSet([1, 2, 3])))
 
 # combinations with repetition
-for i in your_combinations.combinations(2, True):
-    print(i)
+print("Combinations with repetition:", list(your_combinations.combinations(2, True)))
 
 # combinations without repetition
-for i in your_combinations.combinations(2):
-    print(i)
+print("Combinations without repetition:", list(your_combinations.combinations(2)))
 
 # permutations with repetition
-for i in your_combinations.permutations(2, True):
-    print(i)
+print("Permutations with repetition:", list(your_combinations.permutations(2, True)))
 
 # permutations without repetition
-for i in your_combinations.permutations(2):
-    print(i)
+print("Permutations without repetition:", list(your_combinations.permutations(2)))
+
+##################################################################################
+
+# power set
+# for i in your_combinations.powerSet([1, 2, 3]):
+#     print(i)
+
+# combinations with repetition
+# for i in your_combinations.combinations(2, True):
+#     print(i)
+
+# combinations without repetition
+# for i in your_combinations.combinations(2):
+#     print(i)
+
+# permutations with repetition
+# for i in your_combinations.permutations(2, True):
+#     print(i)
+
+# permutations without repetition
+# for i in your_combinations.permutations(2):
+#     print(i)
